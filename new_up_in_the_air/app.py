b27deacd2dd9f72577f357874ced7f69c2225107
idonacmias <idonacmi@gmail.com>
2022-11-22 21:42:27 +0200

    add basic flask app and sqlalcemy db copied from flask and flask-sqlalchemy.

---
+++ new_up_in_the_air/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+# create the extension
+db = SQLAlchemy()
+# create the app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+    
+if __name__ == '__main__':
+	app.run()
